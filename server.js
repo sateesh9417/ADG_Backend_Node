3ab97a262e5470eee0fcf4e79f3ee67c5ddf2fda
sateesh9417 <mambakamsateesh234@gmail.com>
2023-12-12 08:34:37 +0530

    res header removed

--- server.js
+++ server.js
@@ -21,14 +21,6 @@ app.get('/text',(req, res)=>{
 
 app.post('/generate-doc', async (req, res) => {
     try {
-        res.setHeader('Access-Control-Allow-Credentials', true);
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-        res.setHeader(
-            'Access-Control-Allow-Headers',
-            'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-        );
-
         const { html } = req.body;
 
         if (!html) {
